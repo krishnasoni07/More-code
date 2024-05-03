9d412a757c4941c845aa8a5ad5ffbe7179220f5d
kriss-soni <109200209+kriss-soni@users.noreply.github.com>
2024-05-03 22:05:18 +0530

    More code

--- LambdaExpressions/L1.java
+++ LambdaExpressions/L1.java
@@ -11,22 +11,4 @@ interface Shaina extends Krishna,Naina{
     void OurLove();
 }
 
-public class L1 {
-    public static void main(String[] args) {
-        Shaina Kid = new Shaina(){
-            public void HisLove(){
-                System.out.println("Love of Naina");
-            }
-            public void HerLove(){
-                System.out.println("Love of Krishna");
-            }
-            public void OurLove(){
-                System.out.println("Daughter of Krishna and Naina");
-            }
-        };
 
-        Kid.HerLove();
-        Kid.HisLove();
-        Kid.OurLove();
-    }
-}
