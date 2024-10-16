c610ad2fd05d1c5a54321b73b06d99e67b881a9c
kriss-soni <109200209+kriss-soni@users.noreply.github.com>
2024-10-16 16:13:31 +0530

    Temp

---
+++ Practice.java
@@ -0,0 +1,47 @@
+public class Practice {
+
+    public static void main(String args[]){
+
+        int n = 6;
+
+        System.out.println("Ans - ?\n    Why{\n\n}");
+
+        int a = 100;
+        System.out.println(-a++);
+
+        // for(int i = 1; i <= n * 2 - 1;i++){
+        //     for(int j = 1; j <= n * 2 - 1;j++)
+        //         if( (i <= n && j >= n * 2 - i) || ( i > n && j >= n + (i - n) ) )
+        //             System.out.print("*");
+        //         else System.out.print(" ");
+        //     System.out.println();
+        // }       
+    }
+    
+}
+
+
+
+
+
+
+// for(int i = 1; i <= n * 2 - 1; i++){
+//     for(int j = 1; j <= n;j++)
+//         if( ( i <= n && j <= i ) || ( i > n && j <= n - (i - n) ) )
+//             System.out.print("*");
+//         else System.out.print(" ");
+//     System.out.println();
+// }
+
+
+
+
+// for(int i = 1; i <= n; i++){
+
+//     for(int j = 1; j <= n * 2 - 1;j++)
+//         if( j == i || i == 1 || j == n * 2 - i )
+//             System.out.print("*");
+//         else System.out.print(" ");
+//     System.out.println();
+
+// }
